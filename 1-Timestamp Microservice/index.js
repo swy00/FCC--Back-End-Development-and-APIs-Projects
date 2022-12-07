802bb25d5a1aa0900f57f2fc5bfb5ffa287482c4
francisco ronga <franciscoronga00@gmail.com>
2022-12-07 19:39:22 -0300

    TimeStamp Finished

---
+++ 1-Timestamp Microservice/index.js
@@ -0,0 +1,45 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date", function(req, res) {
+  let date = req.params.date;
+  if (date.match(/\d{5,}/)) {
+    date = +date;
+  }
+
+  let fecha = new Date(date);
+  if (fecha.toUTCString() == "Invalid Date") {
+    res.json({ error: fecha.toUTCString() })
+  }
+  res.json({ unix: fecha.valueOf(), utc: fecha.toUTCString() });
+});
+
+app.get("/api/", function(req, res) {
+  let fecha = new Date();//string vacio devuelve fecha actual
+  res.json({ unix: fecha.valueOf(), utc: fecha.toUTCString() });
+})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
